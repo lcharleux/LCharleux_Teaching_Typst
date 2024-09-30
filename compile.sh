68e78f9279889982390a0d756032c63557509fc0
Ludovic Charleux <ludovic.charleux@univ-smb.fr>
2024-09-30 18:37:39 +0200

    Add compile script for generating PDF documentation

---
+++ compile.sh
@@ -0,0 +1,3 @@
+mkdir doc
+typst compile  src/courses/MECA510_Statique/1_rappels/MECA510_Statique_block1_Rappels.typ doc/MECA510_Statique_block1_Rappels.pdf --root=./
+typst compile src/sandbox/demo.typ doc/demo.pdf --root=../../

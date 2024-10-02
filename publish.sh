0a9ce8862cbfe7b173766400b4acdff1e3eba28b
Ludovic Charleux <ludovic.charleux@univ-smb.fr>
2024-10-02 14:56:39 +0200

    Updated publish.sh and README.md

--- publish.sh
+++ publish.sh
@@ -1,5 +1,3 @@
-mkdir doc
-typst compile  src/courses/MECA510_Statique/1_rappels/MECA510_Statique_block1_Rappels.typ doc/MECA510_Statique_block1_Rappels.pdf --root=./
-typst compile src/sandbox/demo.typ doc/demo.pdf --root=../../
+./compile.sh
 
 ghp-import -o -p -n -b=outputs doc
